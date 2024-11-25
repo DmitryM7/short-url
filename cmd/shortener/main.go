20d19a79abbfb6ec3a56ad39c0a2748d07529c81
Maslov Dmitry <dmitrii_m@inbox.ru>
2024-11-25 11:04:37 +0300

    Реализовал функцию  расчета хэша для ссылки.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"hash/crc32"
 	"io"
 	"net/http"
 	"strings"
 )
 
-func createShortURL(url string) string {
-	return url
+var linkTable map[string]string
+
+func createShortURL(url string) (string, error) {
+	var shortURL string
+
+	checksum := crc32.Checksum([]byte(url), crc32.MakeTable(crc32.IEEE))
+
+	shortURL = fmt.Sprintf("%08x", checksum)
+
+	return shortURL, nil
 }
 
 func getURL(id string) (string, error) {
-	return id, nil
+
+	if url, ok := linkTable[id]; ok {
+		return url, nil
+	}
+
+	return "", errors.New("НЕТ ССЫЛКИ ДЛЯ ID")
+
+}
+
+func actionError(w http.ResponseWriter, e string) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(e))
 }
 
 func actionCreateURL(w http.ResponseWriter, r *http.Request) {
@@ -30,7 +52,13 @@ func actionCreateURL(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	newURL := createShortURL(url)
+	newURL, err := createShortURL(url)
+
+	if err != nil {
+		actionError(w, "Не удалось создать короткую ссылку")
+	}
+
+	linkTable[newURL] = url
 
 	w.Header().Set("Content-type", "text/plain")
 	w.WriteHeader(http.StatusCreated)
@@ -40,11 +68,12 @@ func actionCreateURL(w http.ResponseWriter, r *http.Request) {
 
 func actionRedirect(w http.ResponseWriter, r *http.Request) {
 
-	if err := r.ParseForm(); err != nil {
-		actionError(w, "Не смог получить обязательные параметры запроса")
-	}
-
 	id := strings.TrimPrefix(r.URL.Path, "/")
+
+	if id == "" {
+		actionError(w, "Отсутствует id короткой ссылки")
+		return
+	}
 
 	newURL, err := getURL(id)
 
@@ -56,25 +85,24 @@ func actionRedirect(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, newURL, http.StatusTemporaryRedirect)
 }
 
-func actionError(w http.ResponseWriter, e string) {
-	w.WriteHeader(http.StatusBadRequest)
-	w.Write([]byte(e))
-}
-
 func mainEndPoint(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 
 	case http.MethodPost:
 		actionCreateURL(w, r)
+
 	case http.MethodGet:
 		actionRedirect(w, r)
+
 	default:
 		actionError(w, "Отсутствует необходимый end-point.")
 	}
 }
 
 func main() {
+
+	linkTable = make(map[string]string, 100)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", mainEndPoint)
