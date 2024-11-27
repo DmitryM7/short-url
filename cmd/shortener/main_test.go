042970ba0be5f43b70cca4fd81559198c2471073
Maslov Dmitry <dmitrii_m@inbox.ru>
2024-11-27 22:17:58 +0300

    Добавил проверку метода создания короткой ссылки.

---
+++ cmd/shortener/main_test.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestActionCreateURL(t *testing.T) {
+
+	linkTable = make(map[string]string, 100)
+
+	type args struct {
+		method string
+		url    string
+		body   string
+	}
+	type want struct {
+		statusCode int
+		body       string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want want
+	}{
+		{
+			name: "Bad method",
+			args: args{
+				method: http.MethodGet,
+				url:    "/",
+			},
+			want: want{
+				statusCode: http.StatusBadRequest,
+			},
+		},
+		{
+			name: "Bad url",
+			args: args{
+				method: http.MethodPost,
+				url:    "/123",
+			},
+			want: want{
+				statusCode: http.StatusBadRequest,
+			},
+		},
+		{
+			name: "GOOD",
+			args: args{
+				method: http.MethodPost,
+				url:    "/",
+				body:   "www.ya.ru",
+			},
+			want: want{
+				statusCode: http.StatusCreated,
+				body:       "http://localhost:8080/b8da4f2d",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			var r *http.Request
+
+			if tt.args.body != "" {
+				reqBody := strings.NewReader(tt.args.body)
+				r = httptest.NewRequest(tt.args.method, tt.args.url, reqBody)
+			} else {
+				r = httptest.NewRequest(tt.args.method, tt.args.url, nil)
+			}
+
+			w := httptest.NewRecorder()
+			actionCreateURL(w, r)
+			res := w.Result()
+			defer res.Body.Close()
+
+			answer, err := io.ReadAll(res.Body)
+			require.NoError(t, err)
+
+			assert.Equal(t, tt.want.statusCode, res.StatusCode)
+
+			if tt.want.body != "" {
+				assert.Equal(t, tt.want.body, string(answer))
+			}
+		})
+	}
+}
+
+func TestActionRedirect(t *testing.T) {
+	type args struct {
+		method string
+		url    string
+	}
+
+	type want struct {
+		statusCode int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want want
+	}{
+		{
+			name: "Bad method",
+			args: args{
+				method: http.MethodPost,
+				url:    "/",
+			},
+			want: want{
+				statusCode: http.StatusBadRequest,
+			},
+		},
+		{
+			name: "Bad url",
+			args: args{
+				method: http.MethodGet,
+				url:    "/",
+			},
+			want: want{
+				statusCode: http.StatusBadRequest,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := httptest.NewRequest(tt.args.method, tt.args.url, nil)
+			w := httptest.NewRecorder()
+			actionCreateURL(w, r)
+			res := w.Result()
+			assert.Equal(t, tt.want.statusCode, res.StatusCode)
+		})
+	}
+}
