505fc418df7debe2631029dba54e73b312ba0ffb
Maslov Dmitry <dmitrii_m@inbox.ru>
2024-11-28 15:49:33 +0300

    Исправил пропущенную строку flag.Parse()

--- cmd/shortener/config.go
+++ cmd/shortener/config.go
@@ -10,4 +10,5 @@ var (
 func parseFlags() {
 	flag.StringVar(&bndAdd, "a", "localhost:8080", "адрес на котором запускается сервис")
 	flag.StringVar(&retAdd, "b", "localhost:8080", "адрес который возвращается после создания короткого алиаса")
+	flag.Parse()
 }
