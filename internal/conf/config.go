d5bfc9734664a243a7e07cd84e7931d462d5cfcc
Maslov Dmitry <dmitrii_m@inbox.ru>
2024-12-28 08:32:13 +0300

    Добавил модули.

---
+++ internal/conf/config.go
@@ -0,0 +1,33 @@
+package conf
+
+import (
+	"flag"
+	"os"
+)
+
+var (
+	BndAdd   string
+	RetAdd   string
+	FilePath string
+)
+
+func ParseFlags() {
+	flag.StringVar(&BndAdd, "a", "localhost:8080", "адрес на котором запускается сервис")
+	flag.StringVar(&RetAdd, "b", "http://localhost:8080", "адрес который возвращается после создания короткого алиаса")
+	flag.StringVar(&FilePath, "f", "./repo.json", "путь к файлу в котором будет хранится таблица соответствия коротких и полных ссылок")
+
+}
+
+func ParseEnv() {
+	if env := os.Getenv("SERVER_ADDRESS"); env != "" {
+		BndAdd = env
+	}
+
+	if env := os.Getenv("BASE_URL"); env != "" {
+		RetAdd = env
+	}
+
+	if env := os.Getenv("FILE_STORAGE_PATH"); env != "" {
+		FilePath = env
+	}
+}
