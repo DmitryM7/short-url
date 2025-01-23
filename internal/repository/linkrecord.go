3b935f882a5ca90f35acb97527e2455714350530
Maslov Dmitry <dmitrii_m@inbox.ru>
2025-01-23 22:13:39 +0300

    еще испр

--- internal/repository/linkrecord.go
+++ internal/repository/linkrecord.go
@@ -1,7 +1,7 @@
 package repository
 
 type LinkRecord struct {
-	UserID        int    `json:"UserID"`
+	UserID        int    `json:"-"`
 	CorrelationID string `json:"-"`
 	ShortURL      string `json:"ShortURL"`
 	URL           string `json:"OriginalURL"`
