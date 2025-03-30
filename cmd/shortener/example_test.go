d02f3b52714ade3706697cab6bb01c422f380de7
Maslov Dmitry <dmitrii_m@inbox.ru>
2025-03-30 23:22:23 +0300

    Добавил example

---
+++ cmd/shortener/example_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+
+	_ "net/http/pprof"
+
+	"github.com/DmitryM7/short-url.git/internal/conf"
+	"github.com/DmitryM7/short-url.git/internal/controller"
+	"github.com/DmitryM7/short-url.git/internal/logger"
+	"github.com/DmitryM7/short-url.git/internal/repository"
+)
+
+func Examplemain() {
+	lg := logger.NewLogger()
+
+	lg.Infoln("RUN...")
+
+	conf.ParseFlags()
+	flag.Parse()
+	conf.ParseEnv()
+
+	repoConf := repository.StorageConfig{Logger: lg}
+
+	if conf.DSN != "" {
+		repoConf.StorageType = repository.DBType
+		repoConf.DatabaseDSN = conf.DSN
+	} else {
+		repoConf.StorageType = repository.FileType
+		repoConf.FilePath = conf.FilePath
+	}
+
+	repo, err := repository.NewStorageService(repoConf)
+
+	if err != nil {
+		if repoConf.StorageType == repository.FileType {
+			repoConf.StorageType = repository.MemType
+			repo, err = repository.NewStorageService(repoConf)
+
+			if err != nil {
+				lg.Fatalln(err)
+			}
+		} else {
+			lg.Fatalln(err)
+		}
+	}
+
+	r := controller.NewRouter(lg, repo)
+
+	lg.Infoln("Starting server", "bndAdd", conf.BndAdd)
+	lg.Debugln("Conf params:", fmt.Sprintf("%#v", repoConf))
+
+	server := &http.Server{
+		Addr:         conf.BndAdd,
+		Handler:      r,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+	}
+
+	if errServ := server.ListenAndServe(); errServ != nil {
+		lg.Fatalw(errServ.Error(), "event", "start server")
+	}
+}
